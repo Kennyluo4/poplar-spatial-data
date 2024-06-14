2fc66713c57b16072c6e0f90d9566ea9bd2b77e6
ziliangluo <kennyluo4@gmail.com>
2024-06-14 14:59:32 -0400

    add read data cache

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,28 +6,34 @@ import boto3
 
 # Load environment variables from .env file if needed
 # load_dotenv()
-st.balloons()
+# st.balloons()
 
 # Read AWS credentials from environment variables
-aws_access_key_id = st.secrets['AWS_ACCESS_KEY_ID']
-aws_secret_access_key = st.secrets['AWS_SECRET_ACCESS_KEY']
+@st.cache_data
+def get_adata():
+    aws_access_key_id = st.secrets['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key = st.secrets['AWS_SECRET_ACCESS_KEY']
 
-# Initialize S3 client
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    # Initialize S3 client
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
-bucket_name = 'testzl57208'
-file_key = 'poplar_axl_bud_A_adata.h5ad'
-local_file_name = 'poplar_axl_bud_A_adata.h5ad'
+    bucket_name = 'testzl57208'
+    file_key = 'poplar_axl_bud_A_adata.h5ad'
+    local_file_name = 'poplar_axl_bud_A_adata.h5ad'
+    # print(f'Reading file: {local_file_name}')
 
-# Download file from S3
-s3.download_file(bucket_name, file_key, local_file_name)
+    # Download file from S3
+    s3.download_file(bucket_name, file_key, local_file_name)
 
-# Read the AnnData file using Scanpy
-adata1 = sc.read(local_file_name)
-# adata1 = sc.read_h5ad('')
+    # Read the AnnData file using Scanpy
+    adata1 = sc.read(local_file_name)
+    return adata1
+
+adata = get_adata()
+# adata1 = sc.read_h5ad('/Users/ziliangluo/Library/CloudStorage/OneDrive-UniversityofGeorgia/workspace/poplar_spatial_atlas/saved_obj/poplar_axl_bud_A_adata.h5ad')
 
 # Get the list of gene IDs
-gene_ids = adata1.var.index.tolist()
+gene_ids = adata.var.index.tolist()
 
 # Sidebar for plot type selection and gene name input
 st.sidebar.header('Plot Configuration')
@@ -48,7 +54,7 @@ if plot_type == 'UMAP':
     if len(variables_to_plot) == 1:
         axs = [axs]
     for ax, gene in zip(axs, variables_to_plot):
-        sc.pl.umap(adata1, color=gene, ax=ax, show=False, wspace=0.6)
+        sc.pl.umap(adata, color=gene, ax=ax, show=False, wspace=0.6)
     st.pyplot(fig)
 elif plot_type == 'Spatial':
     st.subheader('Spatial Plot')
@@ -56,7 +62,7 @@ elif plot_type == 'Spatial':
     if len(variables_to_plot) == 1:
         axs = [axs]
     for ax, gene in zip(axs, variables_to_plot):
-        sc.pl.spatial(adata1, color=gene, ax=ax, show=False, wspace=0.6)
+        sc.pl.spatial(adata, color=gene, ax=ax, show=False, wspace=0.6)
     st.pyplot(fig)
 else:
     st.write("Select a plot type from the sidebar to begin.")
